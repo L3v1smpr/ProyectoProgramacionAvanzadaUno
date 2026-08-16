2289102a9b9e7b4d404943e0be30b253c8b20f80
L3v1smpr <elvispenaloza23s@gmail.com>
2026-08-16 18:40:20 -0400

    Main.java terminado, pueden ser realizado cambios si se necesita. Creación del bucle while, pregunta hasta que la opción sea válida. Utilización de try-catch para manejo de excepciones de entrada y errores. Ejecución de ventana o consola añadida correctamente.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,6 @@
 package main;
+import vista.MenuConsola;
+import vista.MenuVentana;
 import controlador.SistemaClub;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,23 +14,43 @@ public class Main {
 		
 		SistemaClub controlador = new SistemaClub();
 		BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
-		boolean opcionValida = false;
 		
+		boolean opcionValida = false;
+		String lineaLeida = "";
 		
 		System.out.println("=== BIENVENIDO AL SISTEMA DEL CLUB ===");
 		
 		while (!opcionValida) {
+
 			System.out.println("¿Qué interfaz desea utilizar?");
 			System.out.println("1. Modo Consola (Texto)");
 			System.out.println("2. Modo Ventana (Gráfico)");
 			System.out.println("Ingrese una opción:");
-		
-		
-		
-		
-		
+			
+			try {
+				lineaLeida = lector.readLine();
+				
+				if (lineaLeida != null && (lineaLeida.equals("1") || lineaLeida.equals("2"))) {
+					opcionValida = true;
+				} else {
+					System.out.println("Opción inválida. Seleccione 1 o 2.");
+				}
+			} catch (IOException error) {
+				System.out.println("Error: " + error.getMessage());
+			}
 		}
-	
+		
+		
+		if (lineaLeida.equals("1")) {
+			System.out.println("Ejecutando modo consola...");
+			MenuConsola menuConsola = new MenuConsola(controlador);
+			menuConsola.iniciarConsola();
+			
+		} else {
+			System.out.println("Ejecutando modo ventana...");
+			MenuVentana menuVentana = new MenuVentana(controlador);
+			menuVentana.iniciarVentana();
+		}
 	}
 	
 }
