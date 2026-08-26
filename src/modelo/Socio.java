d724fe5c6e53257ba67dbaa98544b8f5d8aeff89
NotChinoski <cristianemericavalosa@gmail.com>
2026-08-26 00:34:07 -0400

    Implementacion de estructura base y encapsulamiento en Socio

--- src/modelo/Socio.java
+++ src/modelo/Socio.java
@@ -13,63 +13,63 @@ public class Socio {
 	//Constructor
 	
 	public Socio(String rut, String nombre, int edad, int deuda, boolean esMoroso) {
-		
+		this.rut = rut;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.deuda = deuda;
+		this.esMoroso = esMoroso;
+		this.arrayReservas = new ArrayList<>();
 	}
 	
 	//Getters
 	public String getRut() {
-		
+		return rut;
 	}
 	
 	public String getNombre() {
-		
+		return nombre;
 	}
 	
 	public int getEdad() {
-		
+		return edad;
 	}
 	
 	public int getDeuda() {
-		
+		return deuda;
 	}
 	
 	public boolean getIsMoroso() {
-		
+		return esMoroso;
 	}
-	
-	
-	
-	
-	
 	
 	//Setters
 	public void setRut(String rut) {
-		
+		this.rut = rut;
 	}
 	
 	public void setNombre(String nombre) {
-		
+		this.nombre = nombre;
 	}
 	
 	public void setEdad(int edad) {
-		
+		this.edad = edad;
 	}
 	
 	public void setDeuda(int deuda) {
-		
+		this.deuda = deuda;
 	}
 	
-	public void setEsMoroso(boolean estado) {
-		
+	public void setEsMoroso(boolean esMoroso) {
+		this.esMoroso = esMoroso;
 	}
 	
 	
-	//Sobrecarga - Si se ingresa un valor inferior a la deuda total, es solo un abono a la deuda, no deja de ser moroso
+	//Sobrecarga - Si se ingresa un valor inferior a la deuda total, es solo un abono a la deuda, no deja de ser moroso.
 	public void abonarDeuda(int monto) {
 		
 	}
 	
-	//Si no se ingresa un valor, salda la deuda completa, deja de ser moroso
+	//Si no se ingresa un valor, salda la deuda completa, deja de ser moroso.
 	public void abonarDeuda() {
 		
 	}
