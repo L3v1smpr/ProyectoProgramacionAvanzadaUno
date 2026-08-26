b25556f1c8dcefaca1212313a5113a466b9edd66
NotChinoski <cristianemericavalosa@gmail.com>
2026-08-26 14:55:27 -0400

    Implementacion de entidad Reserva con soporte para EstadoReserva

--- src/modelo/Reserva.java
+++ src/modelo/Reserva.java
@@ -4,49 +4,58 @@ import java.util.Date;
 public class Reserva {
 	private int idReserva;
 	private Date fecha;
-	private boolean estado;
+	private EstadoReserva estado;
 	private String rutSocio, idActividad;
+	
+	//Constructor
+	public Reserva(int idReserva, Date fecha, EstadoReserva estado, String rutSocio, String idActividad) {
+		this.idReserva = idReserva;
+		this.fecha = fecha;
+		this.estado = estado;
+		this.rutSocio = rutSocio;
+		this.idActividad = idActividad;
+	}
 	
 	
 	//Getters
 	public int getIdReserva() {
-		
+		return idReserva;
 	}
 	
 	public Date getFecha() {
-		
+		return fecha;
 	}
 	
-	public boolean getEstado() {
-		
+	public EstadoReserva getEstado() {
+		return estado;
 	}
 	
 	public String getRutSocio() {
-		
+		return rutSocio;
 	}
 	
 	public String getIdActividadEnReserva() {
-		
+		return idActividad;
 	}
 	
 	//Setters
-	public void setIdReserva(int id) {
-		
+	public void setIdReserva(int idReserva) {
+		this.idReserva = idReserva;
 	}
 	
 	public void setFecha(Date fecha) {
-		
+		this.fecha = fecha;
 	}
 	
-	public void setEstado(boolean estado) {
-		
+	public void setEstado(EstadoReserva estado) {
+		this.estado = estado;
 	}
-	public void setRutSocio(String rut) {
-		
+	public void setRutSocio(String rutSocio) {
+		this.rutSocio = rutSocio;
 	}
 	
-	public void setIdActividadEnReserva(String rut) {
-		
+	public void setIdActividadEnReserva(String idActividad) {
+		this.idActividad = idActividad;
 	}
 	
 }
