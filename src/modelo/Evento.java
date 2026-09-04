11d140b981f3fddcad02e5ab91f34f835332ebdd
L3v1smpr <elvispenaloza23s@gmail.com>
2026-09-04 19:25:22 -0400

    Implementación de constructor, getters, setters y sobrecarga a la hora de mostrar datos

---
+++ src/modelo/Evento.java
@@ -0,0 +1,56 @@
+package modelo;
+import java.util.Date;
+
+public class Evento extends Actividad {
+	
+	private Date fecha;
+	private String lugar;
+	private String tipoEvento;
+	
+	public Evento(String idActividad, String nombre, int cupoMaximo, int edadMinima, Date fecha, String lugar, String tipoEvento){
+		super(idActividad, nombre, cupoMaximo, edadMinima);
+
+        this.fecha = fecha;
+        this.lugar = lugar;
+        this.tipoEvento = tipoEvento;
+	}
+	
+	//Getters
+	
+	public Date getFecha() {
+        return fecha;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public String getTipoEvento() {
+        return tipoEvento;
+    }
+
+
+	//Setters
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+    
+    public void setTipoEvento(String tipoEvento) {
+        this.tipoEvento = tipoEvento;
+    }
+    
+    
+    //Sobrecarga
+    @Override
+    public String mostrarDetalles() {
+        return super.mostrarDetalles()
+            + " | Tipo: " + tipoEvento
+            + " | Fecha: " + fecha
+            + " | Lugar: " + lugar;
+    }
+}
